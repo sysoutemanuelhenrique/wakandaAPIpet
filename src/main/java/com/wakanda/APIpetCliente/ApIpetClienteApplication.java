5824961b8ee1a266725330485c7afc1c9e25f1ca
sysoutemanuelhenrique <emanuel2017.henrique@gmail.com>
2025-01-14 15:02:46 -0300

    primeiro commit

---
+++ src/main/java/com/wakanda/APIpetCliente/ApIpetClienteApplication.java
@@ -0,0 +1,13 @@
+package com.wakanda.APIpetCliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIpetClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIpetClienteApplication.class, args);
+	}
+
+}
